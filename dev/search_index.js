5954dc87f0ccaedf838fb226fce3b3000e5a1342
Documenter.jl <documenter@juliadocs.github.io>
2021-02-09 01:11:09 +0000

    build based on e252ecf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Brillouin","category":"page"},{"location":"#Brillouin","page":"Home","title":"Brillouin","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Brillouin]","category":"page"}]
+}
