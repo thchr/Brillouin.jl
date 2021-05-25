ad365fad90519fd46e610c1c47fb8f61045cd725
Thomas Christensen <tchr@mit.edu>
2021-05-25 15:09:43 -0400

    extra kwarg options to `plot(::KPathInterpolant, bands)`

--- src/compat/plotlyjs_dispersion.jl
+++ src/compat/plotlyjs_dispersion.jl
@@ -36,12 +36,32 @@ Alternatively, `bands` can be an `AbstractMatrix{<:Real}`, with columns interpre
 distinct energy bands and rows as distinct **k**-points.
 
 ## Keyword arguments `kwargs`
+
 - `ylims`: y-axis limits (default: quasi-tight around `bands`'s range)
+
 - `ylabel`: y-axis label (default: "Energy")
+
+- `band_highlights`: dictionary of non-default styling for specified band ranges (default:
+  `nothing`, indicating all-default styling).
+
+  **Example**: To color bands 2 and 3 black, set
+  `band_highlights = Dict(2:3 => attr(color=:black, width=3)`.
+  Unlisted band ranges are shown in default band styling.
+
+- `annotations`: dictionary of hover-text annotations for labeled high-symmetry points in
+  `kpi` (default: `nothing`, indicating no annotations). Suitable for labeling of irreps.
+
+  **Example**: Assume bands 1 and 2 touch at :X, but not at :Γ. To label this, we set:
+  `annotations = Dict(:X => [1:2 => "touching!], :Γ => [1 => "isolated", 2 => "isolated"])`.
+  If a band-range is provided, a single annotation is placed at the mean of the energies
+  at these band-ranges.
+
 """
-function plot(kpi::KPathInterpolant, bands,#::AVec{<:AVec{<:Real}},
+function plot(kpi::KPathInterpolant, bands,
               layout::Layout = DEFAULT_PLOTLY_LAYOUT_DISPERSION;
-              ylims = nothing, ylabel = "Energy")
+              ylims = nothing, ylabel = "Energy",
+              band_highlights::Union{Dict, Nothing} = nothing,
+              annotations::Union{Dict, Nothing} = nothing)
     # check input
     N = length(kpi)
     if !all(band -> length(band) == N, bands)
@@ -81,15 +101,30 @@ function plot(kpi::KPathInterpolant, bands,#::AVec{<:AVec{<:Real}},
         stop_idx = start_idx+length(local_x)-1
         x = local_x .+ offset_x
         # plot bands
-        for band in bands
-            push!(tbands, 
-                    PlotlyJS.scatter(x=x, y=band[start_idx:stop_idx],
-                        hoverinfo="y", mode="lines", line=attr(color=BAND_COL[], width=3)))
+        for (i, band) in enumerate(bands)
+            line = something(_get_value_if_in_ranges(band_highlights, i), 
+                             attr(color=BAND_COL[], width=3)) # default            
+            push!(tbands, PlotlyJS.scatter(x=x, y=band[start_idx:stop_idx],
+                                           hoverinfo="y", mode="lines", line=line))
         end
         # define xticks
         for (idx, lab) in labels
             xticks[i+=1] = x[idx]
             xlabs[i]     = lab
+        end
+        # place any high-symmetry point annotations
+        if annotations !== nothing
+            for (lab, bandidxs_and_strs) in annotations
+                idx = findfirst(==(Symbol(lab)), labels)
+                idx == nothing && continue
+                for (bandidxs, str) in bandidxs_and_strs
+                    freq = sum(b->bands[b][idx + start_idx - 1], bandidxs)/length(bandidxs)                   
+                    push!(tbands,
+                            PlotlyJS.scatter(x = x[idx:idx], y=freq:freq,
+                                hoverinfo="text", hovertext=str, mode="marker",
+                                line=attr(color=:black)))
+                end                
+            end
         end
         # draw boxes
         xlims = extrema(x)
@@ -126,3 +161,11 @@ function default_dispersion_ylims(bands)
     end
     return ylims
 end
+
+function _get_value_if_in_ranges(d::Dict, i::Integer)
+    for (k, v) in d
+        i ∈ k && return v
+    end
+    return nothing
+end
+_get_value_if_in_ranges(::Nothing, ::Integer) = nothing
