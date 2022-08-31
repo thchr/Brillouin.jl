6b3c57bb83f9faf48f3dbbb47921a656d1c8f51e
Thomas Christensen <tchr@mit.edu>
2022-08-30 21:50:26 -0400

    add labels to `Makie.plot!(::KPath)` (fix #10)

--- src/requires/makie_kpaths.jl
+++ src/requires/makie_kpaths.jl
@@ -1,3 +1,6 @@
+using LinearAlgebra
+using .StaticArrays: normalize
+
 # ---------------------------------------------------------------------------------------- #
 # `KPath`
 # NB: This file expects to be loaded *after* /src/requires/makie_wignerseitz.jl
@@ -6,38 +9,52 @@
     Attributes(
         linecolor = KPATH_COL[],
         linewidth = 3,
+        linekws = (;),
         markercolor = KPATH_COL[],
         markersize = 20,
-        marker = :circle
+        markerkws = (;),
+        textcolor = :black,
+        textkws = (;)
     )
 end
 
 function Makie.plot!(kpp::KPathPlot{Tuple{KPath{D}}}) where D
     okp = kpp[1] # NB: an `Observable{KPath{D}}` not a `KPath{D}`, so needs [] to access
 
-    segments = Makie.Observable(Makie.Point{D,Float64}[])
-    pts      = Makie.Observable(Makie.Point{D,Float64}[])
+    segments  = Makie.Observable(Makie.Point{D,Float64}[])
+    pts       = Makie.Observable(Makie.Point{D,Float64}[])
+    labels    = Makie.Observable(String[])
+    labelspos = Makie.Observable(Makie.Point{D,Float64}[])
+
     function update_plot!(kp)
         empty!(segments[])
+        empty!(pts[])
+        empty!(labels[])
+        empty!(labelspos[])
+
         setting(kp) !== CARTESIAN && (kp = cartesianize(kp))
         for (i,path) in enumerate(paths(kp))
             kvs = [points(kp)[lab] for lab in path]
             append!(segments[], kvs)
             i ≠ length(paths(kp)) && push!(segments[], Point{D,Float64}(NaN)) # separator
         end
-
-        for (i,(lab, kv)) in enumerate(points(kp))
-            push!(pts[], kv)
-            # TODO: k-labels on hover?
-        end
-        pts[]=pts[] # trigger observables
+        pts[] = collect(values(points(kp))) # update and trigger observables
+        labels[] = [string(klab) for klab in keys(points(kp))]
+        labelspos[] = eval_label_positions(pts[], basis(kp))
     end
     Makie.Observables.onany(update_plot!, okp) # update plot if `oc` observable changes
-    update_plot!(okp[]) # populate `segments` and `pts` for initial call
+    update_plot!(okp[]) # populate `segments`, `pts`, `labels`, `labelspos` for initial call
 
-    Makie.lines!(kpp, segments; color = kpp.linecolor, linewidth = kpp.linewidth)
+    Makie.lines!(kpp, segments;
+                 color = kpp.linecolor, linewidth = kpp.linewidth, kpp.linekws...)
     Makie.scatter!(kpp, pts; 
-                   color=kpp.markercolor, markersize=kpp.markersize, marker=kpp.marker)
+                   color=kpp.markercolor, markersize=kpp.markersize, marker=:circle,
+                   kpp.markerkws...)
+    Makie.text!(kpp, labelspos[]; text=labels[],
+                align = (0.5,0.5), fontsize = 22,
+                color = kpp.textcolor, strokewidth = D==3 ? 3.5 : 0, strokecolor = :white,
+                kpp.textkws...)
+
     return kpp
 end
 
@@ -58,3 +75,19 @@ function Makie.plot(kp::Union{Observable{KPath{D}}, KPath{D}};
 
     return Makie.FigureAxisPlot(f, ax, p)
 end
+
+# ---------------------------------------------------------------------------------------- #
+# Utilities
+
+function eval_label_positions(kvs, basis)
+    maxnorm = maximum(norm, basis) # determine offset based on basis norms
+    shiftlen = maxnorm * .065
+    map(kvs) do kv
+        if iszero(kv) # pull Γ _away_ from the mean of all other points
+            kv + normalize(sum(kvs)) * (-shiftlen)
+        else
+            kv + normalize(kv) * shiftlen
+        end
+    end
+end
+
