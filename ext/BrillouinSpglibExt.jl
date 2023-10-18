e9821cb2d8853c8bb12a94c909d7646a40d9a121
Michael F. Herbst <info@michael-herbst.com>
2023-10-18 10:35:52 +0200

    Fixes to BrillouinSpglibExt as suggested in #26 (#27)
    Update Spglib.jl and adjust to new API conventions in Spglib.jl.

--- ext/BrillouinSpglibExt.jl
+++ ext/BrillouinSpglibExt.jl
@@ -20,11 +20,11 @@ associated primitive cell is returned (in the basis of supercell reciprocal latt
 function Brillouin.KPaths.irrfbz_path(cell::Spglib.Cell)
     # extract a standardized primitive basis `pRs` assoc. w/ `cell` via Spglib
     dset = Spglib.get_dataset(cell)
-    sgnum = dset.spacegroup_number
-    pRs = SVector{3}(SVector{3,Float64}(col) for col in eachcol(dset.std_lattice))
+    sgnum = Int(dset.spacegroup_number)
+    pRs = SVector{3}(Spglib.basisvectors(dset.std_lattice))
 
     # print warning if the input cell is a supercell (without any distortion)
-    if !isapprox(det(cell.lattice), det(dset.primitive_lattice)) # check volumes agree
+    if !isapprox(det(parent(cell.lattice)), det(parent(dset.primitive_lattice))) # check volumes agree
         @warn "The provided cell is a supercell: the returned k-path is the standard k-path " *
               "of the associated primitive cell in the basis of the supercell reciprocal lattice." cell
     end
@@ -40,15 +40,15 @@ function Brillouin.KPaths.irrfbz_path(cell::Spglib.Cell)
     #   of the lattice basis vectors.
     # `transformation` is not used, but is commented here just for information. It can be
     # obtained via
-    #   `transformation = inv(primitivebasismatrix(centering(sgnum, 3))) * dset.transformation_matrix'`
+    #   `transformation = inv(primitivebasismatrix(centering(sgnum, 3))) * dset.transformation_matrix`
     rotation = dset.std_rotation_matrix
 
     # Rotate k-points in Cartesian space by `rotation`
-    pRs_original = SVector{3}(SVector{3,Float64}(col) for col in eachcol(cell.lattice))
+    pRs_original = SVector{3}(Spglib.basisvectors(cell.lattice))
     pGs_original = reciprocalbasis(pRs_original)
     cartesianize!(kp)
     for (lab, kv) in points(kp)
-        points(kp)[lab] = rotation * kv
+        points(kp)[lab] = rotation' * kv  # transpose because k coords are in reciprocal space
     end
     return latticize(kp, pGs_original)
 end
