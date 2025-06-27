9201b9a6805c6f605e8a74d8237b83b86b11f9c6
Kipton Barros <kbarros@gmail.com>
2025-06-27 12:01:54 -0600

    More fixes

--- ext/BrillouinMakieExt/kpaths.jl
+++ ext/BrillouinMakieExt/kpaths.jl
@@ -85,8 +85,8 @@ end
 function Makie.plot!(ckpp::CellKPathPlot{Tuple{Cell{D}, KPath{D}}}) where D
     oc  = ckpp[1] # NB:   an `Observable{Cell{D}}` not a `Cell{D}`, so needs [] to access
     okp = ckpp[2] # same: an `Observable{KPath{D}}` not a `KPath{D}`
-    Makie.plot!(ckpp, oc; ckpp.cell...)
-    Makie.plot!(ckpp, okp; ckpp.kpath...)
+    Makie.plot!(ckpp, oc; to_value(ckpp.cell)...)
+    Makie.plot!(ckpp, okp; to_value(ckpp.kpath)...)
     return ckpp
 end
 
